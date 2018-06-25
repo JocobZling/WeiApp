43b8989164eb892dde14ae81fa23f10f2c3ef0d2
Jocobzling <924798429@qq.com>
2018-06-25 10:31:52 +0800

    addDisplayMyClockRouter

--- weiapp/servers/router/displayMyClockRouter.js
+++ weiapp/servers/router/displayMyClockRouter.js
@@ -7,22 +7,26 @@ let Clock = require('../models/clock');
 let requireAuth = require('./requireAuth');
 router.use('/',requireAuth,function (req, res) {
     let clockDetail = [];
-    User.findOne({openid: req.openid}, (err, user) => {
+    console.log(req.openid);
+    User.find({openid: req.openid}, (err, user) => {
         if (err) {
             return console.log(err);
         }
-        for (let item of user.clocks) {
-            Clock.find({id: item.id}, (err, clock) => {
+        for (let item of user[0].clocks) {
+           let ids =parseInt(item.id);
+           Clock.find({id:ids}, (err, clock) => {
                 if (err) {
                     return console.log(err);
                 }
+                console.log("-------------------------");
+                console.log(clock);
                 clockDetail.push({
                     id:clock[0].id,
                     name: clock[0].name,
                     image: clock[0].image,
                     detail: clock[0].detail
                 });
-                if (clockDetail.length === user.clocks.length) {
+                if (clockDetail.length === user[0].clocks.length) {
                     return res.json({
                         clocks: clockDetail,
                     })
